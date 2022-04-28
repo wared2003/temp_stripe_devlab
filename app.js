d13726790376ce11739ca4260860bfcb87a694b7
Edouard <edouard.nicolas@edu.devinci.fr>
2022-04-28 19:41:10 +0200

    f

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ form.addEventListener('submit', async (event) => {
     //`Elements` instance that was used to create the Payment Element
     elements,
     confirmParams: {
-      return_url: window.location.search,
+      return_url: window.location.href,
     },
   });
 
